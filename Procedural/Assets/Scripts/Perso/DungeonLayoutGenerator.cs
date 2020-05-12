7369ba8307e7aee7a214bf4e9598dd87d1c7a202
Dawmuc <vincentpettineo@yahoo.fr>
2020-05-12 08:52:47 +0200

    Init + Package Import

---
+++ Procedural/Assets/Scripts/Perso/DungeonLayoutGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using System.Linq;
+
+public enum ExitEnum
+{
+	Up,
+	Down,
+	Left,
+	Right
+}
+
+public class DungeonLayoutGenerator : MonoBehaviour
+{
+	[SerializeField] private string scriptableDirectoryPath = "Assets/Scripts/Perso/ScriptableRoom";
+	[SerializeField] private int layoutSize = 11;
+	private RoomClass[] possiblesRooms;
+	private RoomClass[,] rooms;
+
+	private void Awake()
+	{
+		// retrieve all possible rooms
+		string[] fileNames = Directory.GetFiles(scriptableDirectoryPath).Where(path => !path.EndsWith(".meta")).ToArray();
+		possiblesRooms = new RoomClass[fileNames.Length];
+		for (int i = 0; i < fileNames.Length; i++) { possiblesRooms[i] = new RoomClass((RoomScriptable)AssetDatabase.LoadAssetAtPath(fileNames[i], typeof(RoomScriptable))); }
+
+		// 
+		if (layoutSize % 2 == 0) layoutSize++;
+		rooms = new RoomClass[layoutSize, layoutSize];
+	}
+}
+
+public class RoomClass
+{
+	public ExitEnum[] exits;
+	public int difficulty;
+
+	public RoomClass(RoomScriptable rs)
+	{
+		exits = rs.exits;
+		difficulty = rs.difficulty;
+	}
+}
