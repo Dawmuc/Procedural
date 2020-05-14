5a2798c8dafe374e0b273b5d412fcf8a36a37ae1
Laure Gilli <acquaviva.laure@gmail.com>
2020-05-14 11:24:32 +0200

    More rooms + moving spikes done

--- Procedural/Assets/Scripts/MoveableSpike.cs
+++ Procedural/Assets/Scripts/MoveableSpike.cs
@@ -11,6 +11,7 @@ public class MoveableSpike : MonoBehaviour
 	public GameObject spike_Gameobject;
 	public int Timer;
 	public bool test = true;
+	private float time = 0;
 
 	private void OnTriggerStay2D(Collider2D collision)
 	{
@@ -26,62 +27,27 @@ public class MoveableSpike : MonoBehaviour
 
 	private void Update()
 	{
-		if (test == true)
-		{
-			Debug.Log("actif");
-			StartCoroutine(Start());
-		}
-		if (test == false)
-		{
-			Debug.Log(" pas actif");
+		time = time + Time.deltaTime;
 
-			StartCoroutine(Start());
+		if (time > Timer)
+		{
+			time = time - Timer;
+			if (test == true)
+			{
+				spike_Gameobject.SetActive(false);
+				test = false;
+
+			}
+
+			else
+			{
+
+				spike_Gameobject.SetActive(true);
+				test = true;
+
+			}
 		}
+
 	}
-	
 
-
-
-	public IEnumerator Start()
-	{
-		
-		//yield return new WaitForSeconds(Timer);
-		
-		if (test == true)
-		{
-			yield return new WaitForSeconds(Timer);
-			spike_Gameobject.SetActive(false);
-			test = false;
-			
-			yield return null;
-		}
-
-		if (test == false)
-		{
-			yield return new WaitForSeconds(Timer);
-			spike_Gameobject.SetActive(true);
-			test = true;
-			yield return null;
-		}
-		
-		/*
-					if
-					//Turn My game object that is set to false(off) to True(on).
-					this.gameObject.SetActive(true);
-
-					//Turn the Game Oject back off after 1 sec.
-					yield return new WaitForSeconds(3);
-
-					//Game object will turn off
-					this.gameObject.SetActive(false);*/
-	}
-	/*
-        if (test ==true)
-        {
-			this.gameObject.SetActive(true);
-        }
-			if (test == false)
-		{
-			this.gameObject.SetActive(false);
-		}*/
 }
